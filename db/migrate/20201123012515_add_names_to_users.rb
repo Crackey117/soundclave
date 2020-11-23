c4a6ea3df1b40e5c60a8a7b509fde50b4c6070b2
Chris Rackey <chris.rackey@gmail.com>
2020-11-22 21:13:53 -0500

    added username, role, first and last name to user

---
+++ db/migrate/20201123012515_add_names_to_users.rb
@@ -0,0 +1,8 @@
+class AddNamesToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false 
+    add_column :users, :username, :string, null: false 
+    add_column :users, :role, :string, null: false, default: "Fan"
+  end
+end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,10 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name { 'firstname' }
+    last_name { 'lastname' }
+    username { 'username' }
+    role { 'role' }
     password { 'password' }
     password_confirmation { 'password' }
   end
