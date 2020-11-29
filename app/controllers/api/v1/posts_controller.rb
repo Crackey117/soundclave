47f3e6775518e1f3d9e640185c4f093f0e40591e
Chris Rackey <chris.rackey@gmail.com>
2020-11-28 22:50:19 -0500

    started adding post functionality for status posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PostsController < ApiController  
-  before_action :authenticate_user, only: [:create]
+  before_action :authenticate_user!, only: [:create]
   def index
     post_items = Post.all
     render json: {
@@ -7,7 +7,21 @@ class Api::V1::PostsController < ApiController
     }
   end
 
+  def create
+    new_post = Post.new(post_params)
+    new_post.user = current_user 
+    if new_post.save
+      render json: new_post
+    else
+      render json: { errors: new_post.errors }
+    end
+  end
   private 
+
+
+  def post_params
+    params.require(:post).permit([:body])
+  end
   def serialized_data(data, serializer)
     ActiveModelSerializers::SerializableResource.new(data, each_serializer: serializer, scope: current_user)
   end
